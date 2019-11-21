7a64d15a06ac264da48d30bd5953e68e10963281
johnhession <john.j.hession90@gmail.com>
2019-11-21 13:35:05 -0800

    Class Proj

---
+++ guiquotetest.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import gi
+import random
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class Aarrons_Inspirational_Quotes(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Quote Central")
+
+        self.buttonbox = Gtk.ButtonBox(spacing=6)
+        self.add(self.buttonbox)
+
+        button1 = Gtk.Button.new_with_label(label="Inspiration")
+        button1.connect("clicked", self.on_info_clicked)
+        self.buttonbox.add(button1)
+
+        button2 = Gtk.Button.new_with_label(label="Motivation")
+        button2.connect("clicked", self.on_error_clicked)
+        self.buttonbox.add(button2)
+
+        button3 = Gtk.Button.new_with_label(label="Dark Humor")
+        button3.connect("clicked", self.on_warn_clicked)
+        self.buttonbox.add(button3)
+
+        button4 = Gtk.Button.new_with_label(label="Aaron said these")
+        button4.connect("clicked", self.on_question_clicked)
+        self.buttonbox.add(button4)
+
+    def on_info_clicked(self, widget):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "Your Inspiration For The Day")
+        keymatch_value = "IN"
+        quote_file = open("quotes.txt", "r")
+        lines = quote_file.readlines()
+        key_match = [s for s in lines if keymatch_value in s]
+        quote = random.choice(key_match)
+        dialog.format_secondary_text(quote)
+        dialog.run()
+        print("INFO dialog closed")
+
+        dialog.destroy()
+
+    def on_error_clicked(self, widget):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, ("Time to get motivated"))
+        keymatch_value = "MO"
+        quote_file = open("quotes.txt", "r")
+        lines = quote_file.readlines()
+        key_match = [s for s in lines if keymatch_value in s]
+        quote = random.choice(key_match)
+        dialog.format_secondary_text(quote)
+        dialog.run()
+        print("INFO dialog closed")
+
+        dialog.destroy()
+
+    def on_warn_clicked(self, widget):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "Give in to the dark side")
+        keymatch_value = "HU"
+        quote_file = open("quotes.txt", "r")
+        lines = quote_file.readlines()
+        key_match = [s for s in lines if keymatch_value in s]
+        quote = random.choice(key_match)
+        dialog.format_secondary_text(quote)
+        dialog.run()
+        print("INFO dialog closed")
+
+        dialog.destroy()
+
+
+    def on_question_clicked(self, widget):
+        text= "If you hate comptia you've come to the right place"
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, text)
+        keymatch_value = "AA"
+        quote_file = open("quotes.txt", "r")
+        lines = quote_file.readlines()
+        key_match = [s for s in lines if keymatch_value in s]
+        quote = random.choice(key_match)
+        dialog.format_secondary_text(quote)
+        dialog.run()
+        print("INFO dialog closed")
+
+        dialog.destroy()
+
+
+
+win = Aarrons_Inspirational_Quotes()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
--- secondapp.py
+++ secondapp.py
@@ -1,22 +1,35 @@
+#!/usr/bin/env python3
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class MyWindow(Gtk.Window):
+class Main(Gtk.Window):
 
     def __init__(self):
-        Gtk.ScrolledWindow.__init__(self, title="Hello World")
+        Gtk.Window.__init__(self, title="Hello World")
 
-        self.box = Gtk.Box(spacing=10)
-        self.add(self.box)
+        self.vbox = Gtk.VBox()
+        self.set_default_size(300, 300)
+        self.add(self.vbox)
+
 
         self.button1 = Gtk.Button(label="Click Here")
         self.button1.connect("clicked", self.on_button1_clicked)
-        self.box.pack_start(self.button1, True, True, 0)
+        self.vbox.pack_start(self.button1, True, True, 0)
 
         self.button2 = Gtk.Button(label="Goodbye")
         self.button2.connect("clicked", self.on_button2_clicked)
-        self.box.pack_start(self.button2, True, True, 0)
+        self.vbox.pack_start(self.button2, True, True, 0)
+
+        self.button3 = Gtk.Button(label="This is my GitHub Address...")
+        self.button3.connect("clicked", self.on_button3_clicked)
+        self.vbox.pack_start(self.button3, True, True, 0)
+
+        self.button4 = Gtk.Button(label="How to get fedora")
+        self.button4.connect("clicked", self.on_button4_clicked)
+        self.vbox.pack_start(self.button4, True, True, 0)
+
 
     def on_button1_clicked(self, widget):
         print("Hello World")
@@ -25,7 +38,14 @@ class MyWindow(Gtk.Window):
     def on_button2_clicked(self, widget):
         print("I can't believe you did that!")
 
-win = MyWindow()
+    def on_button3_clicked(self, widget):
+        print("https://github.com/johnhession/mycode")
+
+    def on_button4_clicked(self, widget):
+        print("https://getfedora.org/")
+
+
+win = Main()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
