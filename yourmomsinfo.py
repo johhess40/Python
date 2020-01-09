3c8b173f36c1fd021ed414f1b24f03337919c730
johnhession <57915310+johnhession@users.noreply.github.com>
2020-01-09 10:38:45 -0800

    Delete yourmomsinfo.py

--- yourmomsinfo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-user_input1 = input("What is your address?:")
-user_input2 = input("What is your mothers maiden name?:")
-user_input3 = input("What is she cooking for dinner?:")
-
-print("So you're telling me that your mothers maiden name is " + user_input2 + ", " + "and that she lives at " + user_input1 + ".... " + "I'd sure love some of that " + user_input3 + "!" )
